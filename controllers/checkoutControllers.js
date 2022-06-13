7e910b80fc9bf1ca88a342bd33cc9f4183f93062
priyanka <priyanka@techgentsia.com>
2022-06-13 10:24:09 +0530

    chehcout list issue solved

--- controllers/checkoutControllers.js
+++ controllers/checkoutControllers.js
@@ -5,119 +5,10 @@ const Token = require("./token");
 
 
 
-const GetBook = (data) => {
-  return new Promise(async (resolve, reject) => {
-   let token = await Token.getToken
-    const req = {
-      method: 'get',
-      url: `${process.env.kohaBaseUrl}/biblios/${data.id}`,
-      headers: {
-        Accept: 'application/json',
-        Authorization: `Bearer ${token}`
-      }
-    }
-    axios(req)
-      .then((resp) => {
-        resolve(resp.data)
-      }).catch((err) => {
-        reject(err)
-      })
-  })
-}
-const PlaceHold = (data) => {
-  return new Promise(async (resolve, reject) => {
-   let token = await Token.getToken
-    //console.log(data)
-    const req = {
-      method: 'post',
-      url: `${process.env.kohaBaseUrl}/holds`,
-      headers: {
-        Accept: 'application/json',
-        Authorization: `Bearer ${token}`
-      },
-      data: data
-    }
-    axios(req)
-      .then((resp) => {
-        resolve(resp.data)
-      }).catch((err) => {
-        if (err.response.status === 403) {
-          reject({
-            Error:'Item already on hold',
-            err,
-          })
-        }
-        if (err.response.status === 400) {
-          reject({
-            Error:'Missing parameters',
-            err
-          })
-        }
-      })
-  })
-}
-const CancelHold = (data) => {
-  return new Promise(async (resolve, reject) => {
-   let token = await Token.getToken
-    //console.log(data)
-    const req = {
-      method: 'post',
-      url: `${process.env.kohaBaseUrl}/holds`,
-      headers: {
-        Accept: 'application/json',
-        Authorization: `Bearer ${token}`
-      },
-      data: data
-    }
-    axios(req)
-      .then((resp) => {
-        resolve(resp.data)
-      }).catch((err) => {
-        if (err.response.status === 403) {
-          reject({
-            Error:'Item already on hold',
-            err,
-          })
-        }
-        if (err.response.status === 400) {
-          reject({
-            Error:'Missing parameters',
-            err
-          })
-        }
-      })
-  })
-}
 
-const CreatePatron = (data) => {
-  return new Promise(async (resolve, reject) => {
-   let token = await Token.getToken
-    //console.log(data)
-    const req = {
-      method: 'post',
-      url: `${process.env.kohaBaseUrl}/patrons`,
-      headers: {
-        Accept: 'application/json',
-        Authorization: `Bearer ${token}`
-      },
-      data: data
-    }
-    axios(req)
-      .then((resp) => {
-        resolve(resp.data)
-      }).catch((err) => {
-        if (err.response.status === 400) {
-          reject({
-            Error: "Bad parameters or Missing parameters"
-          })
-        }
-        reject(err)
-      })
-  })
-}
 const ListCheckouts = () => {
   return new Promise(async (resolve, reject) => {
-   let token = await Token.getToken
+   let token = await Token.getToken();
     const req = {
       method: 'get',
       url: `${process.env.kohaBaseUrl}/checkouts`,
@@ -134,25 +25,7 @@ const ListCheckouts = () => {
       })
   })
 }
-const ListHolds = () => {
-  return new Promise(async (resolve, reject) => {
-   let token = await Token.getToken
-    const req = {
-      method: 'get',
-      url: `${process.env.kohaBaseUrl}/holds`,
-      headers: {
-        Accept: 'application/json',
-        Authorization: `Bearer ${token}`
-      }
-    }
-    axios(req)
-      .then((resp) => {
-        resolve(resp.data)
-      }).catch((err) => {
-        reject(err)
-      })
-  })
-}
+
 const GetCheckout = (data) => {
   return new Promise(async (resolve, reject) => {
    let token = await Token.getToken
@@ -173,63 +46,11 @@ const GetCheckout = (data) => {
       })
   })
 }
-const GetLibrary = (data) => {
-  return new Promise(async (resolve, reject) => {
-   let token = await Token.getToken
-    //console.log(data)
-    const req = {
-      method: 'get',
-      url: `${process.env.kohaBaseUrl}/libraries/${data.id}`,
-      headers: {
-        Accept: 'application/json',
-        Authorization: `Bearer ${token}`
-      },
-    }
-    axios(req)
-      .then((resp) => {
-        resolve(resp.data)
-      }).catch((err) => {
-        if (err.response.status === 400) {
-          reject({
-            Error: "Missing Library Id"
-          })
-        }
-        reject(err)
-      })
-  })
-}
-const UpdateLibrary = (data) => {
-  return new Promise(async (resolve, reject) => {
-   let token = await Token.getToken
-    //console.log(data)
-    const req = {
-      method: 'post',
-      url: `${process.env.kohaBaseUrl}/patrons`,
-      headers: {
-        Accept: 'application/json',
-        Authorization: `Bearer ${token}`
-      },
-      data: data
-    }
-    axios(req)
-      .then((resp) => {
-        resolve(resp.data)
-      }).catch((err) => {
-        if (err.response.status === 400) {
-          reject({
-            Error: "Bad parameters or Missing parameters"
-          })
-        }
-        reject(err)
-      })
-  })
-}
+
+
 
 module.exports = {
   
   ListCheckouts,
-  GetCheckout,
-  ListHolds,
-  GetLibrary,
-  UpdateLibrary
+  GetCheckout
 }
