09b0dfda617d8fb7403058b7f837757cd4177095
priyanka <priyanka@techgentsia.com>
2022-10-11 15:26:44 +0530

    validation of user name removed

--- models/user.js
+++ models/user.js
@@ -11,12 +11,12 @@ const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
         //maxlength: 10,
-        match: /^[a-zA-Z]/,
+       // match: /^[a-zA-Z]/,
     },
     lastName: {
         type: String,
        // maxlength: 10,
-        match: /^[a-zA-Z]/,
+       // match: /^[a-zA-Z]/,
     },
     cardNumber: {
         type: String,
